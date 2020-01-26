9b2fbc480f8215876d0c5242b5e40da2f39b19f5
Dmitry <wdm.dimpin@gmail.com>
2020-01-27 00:29:05 +0500

    .

--- lesson_4.py
+++ lesson_4.py
@@ -1,6 +1,15 @@
 print('Hello, World!') 
 print('Hello, World2!') 
 
+print('Hello, World!'); print('Hello, World2!') 
+
+print('one', end='->')
+print('two')
+# one->two
+
+print(1, 2, 3, sep=' | ')
+# 1!2!3
+
 # однострочный комментарий
 '''
 многострочный
